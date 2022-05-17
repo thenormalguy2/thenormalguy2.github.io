18c526d5bde613f37739b7f04462b200d298f025
TheNormalGuy <kevin@cpandc.com>
2022-05-16 23:15:35 -0700

    Theme and other fixes

--- montagna/script.js
+++ montagna/script.js
@@ -2,21 +2,60 @@
 //   "(prefers-color-scheme: dark)" = false;
 // }, false);
 
-document.querySelector('.theme-toggle-button').addEventListener('click', () => {
-    document.body.classList.toggle('dark')
-})
+// document.querySelector('.theme-toggle-button').addEventListener('click', () => {
+//     document.body.classList.toggle('dark')
+// })
 
-const btn = document.querySelector(".theme-toggle-button");
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+// document.querySelector('.theme-toggle-button').addEventListener('click', () => {
+//   document.body.classList.toggle('light')
+// })
 
-
-btn.addEventListener("click", function () {
-  if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light");
-  } else {
-    document.body.classList.toggle("dark");
-  }
-});
+// const btn = document.querySelector(".theme-toggle-button");
+// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
 
-  
+// btn.addEventListener("click", function () {
+//   if (prefersDarkScheme.matches) {
+//     document.body.classList.toggle("light");
+//   } else {
+//     document.body.classList.toggle("dark");
+//   }
+// });
+
+// const chk = document.getElementById('chk');
+
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+
+  const chk = document.getElementById('chk');
+
+  chk.addEventListener('change', () => {
+    document.body.classList.toggle('light');
+  });
+
+  // floating_btn.addEventListener('click', () => {
+  //     social_panel_container.classList.toggle('light')
+  // });
+
+  // close_btn.addEventListener('click', () => {
+  //   social_panel_container.classList.remove('light')
+  // });
+
+} else {
+
+  const chk = document.getElementById('chk');
+
+  chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+  });
+
+  // floating_btn.addEventListener('click', () => {
+  //     social_panel_container.classList.toggle('dark')
+  // });
+
+  // close_btn.addEventListener('click', () => {
+  //   social_panel_container.classList.remove('dark')
+  // });
+
+}
+
